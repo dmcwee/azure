586790b77bc0a134aa7ebc7aa8a81c8aed3176cd
dmcwee@microsoft.com <dmcwee@microsoft.com>
2018-01-25 17:17:10 -0500

    Updates to get the custom script extensions working

--- SimpleDemo/SimpleDemo/CustomScripts/DCPrep.ps1
+++ SimpleDemo/SimpleDemo/CustomScripts/DCPrep.ps1
@@ -1,25 +1,19 @@
-[CmdletBinding(SupportsShouldProcess=$true)]
-
 <# Custom Script for Windows #>
-Write-Verbose "Stopping ShellHWDetection Service"
-Stop-Service -Name ShellHWDetection
 
-Start-Sleep 2
+Stop-Service -Name ShellHWDetection;
 
-Write-Verbose "Initializing Disk, Creating Partition, and Formatting Volume"
+Start-Sleep 2;
+
 Get-Disk | 
 Where-Object partitionstyle -eq 'raw' | 
 Initialize-Disk -PartitionStyle MBR -PassThru | 
 New-Partition -DriveLetter L -UseMaximumSize | 
-Format-Volume -FileSystem NTFS -NewFileSystemLabel "ADData" -Confirm:$false
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "ADData" -Confirm:$false;
 
-Start-Sleep 2
+Start-Sleep 2;
 
-Write-Verbose "Starting ShellHWDetection Service"
-Start-Service -Name ShellHWDetection
+Start-Service -Name ShellHWDetection;
 
-Write-Verbose "Adding AD Domain Services and Management Tools"
-Add-WindowsFeature -Name ad-domain-services -IncludeManagementTools
+Add-WindowsFeature -Name ad-domain-services -IncludeManagementTools;
 
-Write-Verbose "Adding DNS Services and Management Tools"
-Add-WindowsFeature -Name DNS -IncludeManagementTools
+Add-WindowsFeature -Name DNS -IncludeManagementTools;
