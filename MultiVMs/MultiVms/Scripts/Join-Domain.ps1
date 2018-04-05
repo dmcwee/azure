ffb91f4fafcb6f2f1a6c7d79bde01a74bce055ee
dmcwee@microsoft.com <dmcwee@microsoft.com>
2018-04-05 14:40:16 -0400

    More cleanup of the branch

--- MultiVMs/MultiVms/Scripts/Join-Domain.ps1
+++
@@ -1,6 +0,0 @@
-param([string]$Domain = "fiveguysconsulting.com")
-
-$ipId = (Get-NetAdapter).ifIndex
-Set-DNSClientServerAddress -interfaceIndex $ipId -ServerAddresses ($PrimaryDns,$SecondaryDns)
-
-Add-Computer -DomainName $domain
