fa0b8b6a1a08549617c506286f89d43235ba2e00
dmcwee@microsoft.com <dmcwee@microsoft.com>
2018-05-01 12:37:40 -0400

    Updates to the Advanced Demo to support automatic domain creation

--- AdvancedDemo/AdvancedDemo/DSC/DomainControllerDSC.ps1
+++ AdvancedDemo/AdvancedDemo/DSC/DomainControllerDSC.ps1
@@ -1,25 +1,55 @@
 Configuration Main
 {
-	Param ( [string] $nodeName )
 
-	Import-DscResource -ModuleName PSDesiredStateConfiguration
+	Param ( 
+		[String]$nodeName, 
+		[String]$driveLetter = "L", 
+		[PSCredential] $adminAccount,
+		[String]$domainName,
+		[int] $retryAttempts = "60",
+		[int] $waitTime = "60"
+	)
 
-	Node $nodeName
-  {
-	  WindowsFeature ADDomainServices {
+	Import-DscResource -ModuleName PSDesiredStateConfiguration, StorageDsc, xActiveDirectory
+	[PSCredential ]$Creds = New-Object -TypeName PSCredential ($adminAccount.UserName, $adminAccount.Password)
+
+	Node localhost
+	{
+		WindowsFeature ADDomainServices {
 			Name="AD-Domain-Services"
 			Ensure="Present"
-	  }
+	 	}
 
-	  WindowsFeature DNS {
+		WindowsFeature DNS {
 			Name="DNS"
 			Ensure="Present"
-	  }
+		}
 
-	  WindowsFeature ADTools {
+		WindowsFeature ADTools {
 			Name="RSAT-AD-Tools"
 			Ensure = "Present"
 			IncludeAllSubFeature = $true
-	  }
-	}
+		}
+
+		WaitForDisk Disk2 {
+			DiskId = 2
+			RetryIntervalSec = $waitTime
+			RetryCount = $retryAttempts
+		}
+
+		Disk ADDisk {
+			DiskId = 2
+			DriveLetter = $driveLetter
+			FSLabel = "ADData"
+			DependsOn = "[WaitForDisk]Disk2"
+		}
+		
+		xADDomain CreateDomain {
+			DomainName = $domainName
+            DomainAdministratorCredential = $Creds
+            SafemodeAdministratorPassword = $Creds
+            DnsDelegationCredential = $DNSDelegationCred
+            DependsOn = "[WindowsFeature]ADDomainServices", "[Disk]ADDisk"
+		}
+ 	}
 }
